596dd363ae6b2c25a6ba697833bd33dee1e47c7b
Fred Wenzel <fwenzel@mozilla.com>
2012-07-07 14:02:19 -0700

    Allow pausing the game, and pause it when leaving the window or tab

--- www/js/app.js
+++ www/js/app.js
@@ -7,20 +7,25 @@ require(['jquery'], function($) {
   // Setup requestAnimationFrame
   requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                           window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+  var animationFrameId;
 
-  // Define grid
+  // Global game metadata
   var blocksize = 16;
-  var grid = {
+  var game = {
+    // Block width and height
     width: 32,
-    height: 32
+    height: 32,
+    // Paused?
+    paused: false
   };
 
   // Create the canvas
   var canvas = document.createElement("canvas");
   var ctx = canvas.getContext("2d");
-  canvas.width = grid.width * blocksize;
-  canvas.height = grid.height * blocksize;
-  document.body.appendChild(canvas);
+  canvas.width = game.width * blocksize;
+  canvas.height = game.height * blocksize;
+  document.getElementById('game').appendChild(canvas);
 
   // Background image
   var bgReady = false;
@@ -43,7 +48,9 @@ require(['jquery'], function($) {
     this.speed = 3; // movement in blocks per second
     this.length = 8; // snake length
     this.dir = 2; // direction
-    this.path = [{x: grid.width / 2, y: grid.height / 2}]; // Track snake's movement
+    this.path = [{x: game.width / 2, y: game.height / 2}]; // Track snake's movement
+
+    this.since_last_update = 0; // Time since last update
   }
   Snake.prototype.in_path = function(pos) {
     // Is this position somewhere in our path already?
@@ -57,8 +64,19 @@ require(['jquery'], function($) {
 
 
   // Handle keyboard controls
-  addEventListener("keydown", function (e) {
+  addEventListener("keydown", function(e) {
+    // Pause / unpause?
+    if (e.keyCode == 80) { // "p"
+      pause();
+      return;
+    }
+
+    // No other commands while paused.
+    if (game.paused) return;
+
+    // Handle direction changes.
     if (!(e.keyCode in dirs)) return;
+    e.preventDefault();
 
     // Avoid opposite directions
     var opposites = {1: 3, 2: 4, 3: 1, 4: 2};
@@ -68,19 +86,20 @@ require(['jquery'], function($) {
     snake.dir = dirs[e.keyCode];
   }, false);
 
+
   // Reset game to original state
   function reset() {
     snake = new Snake();
   };
 
+
   // Update game objects
-  var since_last_update = 0; // Time since last update
   function update(modifier) {
-    since_last_update += modifier;
-    if (since_last_update < 1000 / snake.speed) return;  // no update yet
+    snake.since_last_update += modifier;
+    if (snake.since_last_update < 1000 / snake.speed) return;  // no update due yet
 
     // Update due
-    since_last_update -= 1000 / snake.speed;
+    snake.since_last_update -= 1000 / snake.speed;
 
     // Calculate new snake position.
     var old_pos = snake.path[snake.path.length - 1];
@@ -92,22 +111,23 @@ require(['jquery'], function($) {
       case 4: new_pos.x -= 1; break;
     }
 
-    // Did we run into a wall?
-    if (new_pos.x < 0 || new_pos.x >= grid.width ||
-        new_pos.y < 0 || new_pos.y >= grid.height ||
+    // Did we run into a wall or ourselves?
+    if (new_pos.x < 0 || new_pos.x >= game.width ||
+        new_pos.y < 0 || new_pos.y >= game.height ||
         snake.in_path(new_pos)) {
       console.log('zomg');
       reset();
       return;
     }
 
-    // Add new position to path, delete tail if snake length has been reached.
+    // Add new position to path, crop tail if snake length has been reached.
     snake.path.push(new_pos);
     if (snake.path.length > snake.length) {
       snake.path.shift();
     }
 
   };
+
 
   // Draw everything
   function render() {
@@ -122,8 +142,25 @@ require(['jquery'], function($) {
     }
   };
 
+
+  // Pause the game.
+  function pause(force) {
+    if (animationFrameId || force) { // Pause
+      game.paused = true;
+      cancelAnimationFrame(animationFrameId);
+      animationFrameId = null;
+    } else { // Unpause
+      game.paused = false;
+      then = Date.now(); // Reset.
+      animationFrameId = requestAnimationFrame(main);
+    }
+  };
+  // Pause when leaving the screen.
+  window.addEventListener('blur', function() {pause(true)});
+
+
   // The main game loop
-  var main = function () {
+  function main() {
     var now = Date.now();
     var delta = now - then;
 
@@ -131,15 +168,16 @@ require(['jquery'], function($) {
     render();
 
     then = now;
-    requestAnimationFrame(main);
+    if (!game.paused) {
+      animationFrameId = requestAnimationFrame(main);
+    }
   };
 
   // Let's play this game!
   reset();
   var then = Date.now();
-  // Execute as fast as possible
+  // Execute as soon as possible
   main();
-
 
 // End require.js
 });
