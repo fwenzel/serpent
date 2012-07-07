0bcaef5cab33a81441678bc902a92782f0025fbf
Fred Wenzel <fwenzel@mozilla.com>
2012-07-07 13:11:36 -0700

    Version one: can move around and run into self and wall!

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,6 @@
-
-// The code below uses require.js, a module system for javscript:
-// http://requirejs.org/docs/api.html#define
-//
-// You don't have to use require.js, and you can delete all of this if
-// you aren't (make sure to uncomment the script tags in index.html also)
-
-
-// Set the path to jQuery, which will fall back to the local version
-// if google is down
 require.config({
-    paths: {'jquery':
-            ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-             'lib/jquery']}
+    paths: {'jquery': ['lib/jquery']}
 });
-
 var global = this;
 
 require(['jquery'], function($) {
@@ -52,11 +39,20 @@ require(['jquery'], function($) {
   };
 
   // Game objects
-  var snake = {
-    speed: 1, // movement in blocks per second
-    length: 8, // snake length
-    dir: 1 // direction
-  };
+  function Snake() {
+    this.speed = 2.5; // movement in blocks per second
+    this.length = 8; // snake length
+    this.dir = 2; // direction
+    this.path = [{x: grid.width / 2, y: grid.height / 2}]; // Track snake's movement
+  }
+  Snake.prototype.in_path = function(pos) {
+    for (var i in this.path) {
+      var p = this.path[i];
+      if (pos.x == p.x && pos.y == p.y) return true;
+    }
+    return false;
+  }
+  var snake = new Snake();
 
   // Handle keyboard controls
   var keysDown = {};
@@ -74,44 +70,56 @@ require(['jquery'], function($) {
 
   // Reset game to original state
   function reset() {
-    snake.x = grid.width / 2;
-    snake.y = grid.height / 2;
+    snake = new Snake();
   };
 
   // Update game objects
+  var since_last_update = 0; // Time since last update
   function update(modifier) {
+    since_last_update += modifier;
+    if (since_last_update < 1000 / snake.speed) return;  // no update yet
 
-    // Are they touching?
-    if (false) {
-      reset();
+    // Update due
+    since_last_update -= 1000 / snake.speed;
+
+    // Calculate new snake position.
+    var old_pos = snake.path[snake.path.length - 1];
+    var new_pos = {x: old_pos.x, y: old_pos.y};
+    switch (snake.dir) {
+      case 1: new_pos.y -= 1; break;
+      case 2: new_pos.x += 1; break;
+      case 3: new_pos.y += 1; break;
+      case 4: new_pos.x -= 1; break;
     }
+
+    // Did we run into a wall?
+    if (new_pos.x < 0 || new_pos.x >= grid.width ||
+        new_pos.y < 0 || new_pos.y >= grid.height ||
+        snake.in_path(new_pos)) {
+      console.log('zomg');
+      reset();
+      return;
+    }
+
+    // Add new position to path, delete tail if snake length has been reached.
+    snake.path.push(new_pos);
+    if (snake.path.length > snake.length) {
+      snake.path.shift();
+    }
+
   };
 
   // Draw everything
   function render() {
-    /*
-    if (bgReady) {
-      ctx.drawImage(bgImage, 0, 0);
-    }
-    */
+    // Empty the canvas.
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // draw snake
+    // Draw snake
     ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(snake.x * blocksize, snake.y * blocksize, blocksize, blocksize);
-    //ctx.drawImage(heroImage, hero.x, hero.y);
-
-    /*if (monsterReady) {
-      ctx.drawImage(monsterImage, monster.x, monster.y);
-    }*/
-
-    // Score
-    /*
-    ctx.fillStyle = "rgb(250, 250, 250)";
-    ctx.font = "24px Helvetica";
-    ctx.textAlign = "left";
-    ctx.textBaseline = "top";
-    ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
-    */
+    for (var i in snake.path) {
+      ctx.fillRect(snake.path[i].x * blocksize, snake.path[i].y * blocksize,
+                   blocksize, blocksize);
+    }
   };
 
   // The main game loop
@@ -119,7 +127,7 @@ require(['jquery'], function($) {
     var now = Date.now();
     var delta = now - then;
 
-    update(delta / 1000);
+    update(delta);
     render();
 
     then = now;
@@ -132,7 +140,6 @@ require(['jquery'], function($) {
   // Execute as fast as possible
   main();
 
-});
 
-// END REQUIRE.JS CODE
-// Remove all of this if not using require.js
+// End require.js
+});
