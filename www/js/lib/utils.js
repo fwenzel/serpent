534d55858072dd978af23aac209ddba74c9c179d
Fred Wenzel <fwenzel@mozilla.com>
2012-07-13 20:12:42 -0700

    indentation

--- www/js/lib/utils.js
+++ www/js/lib/utils.js
@@ -1,5 +1,5 @@
 define({
-    getRandomInt: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 });
