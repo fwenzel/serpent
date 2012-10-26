a3af3ea825660631ece94cfd909abd4e67f79292
Fred Wenzel <fwenzel@mozilla.com>
2012-10-25 17:21:30 -0700

    Remember speed in localStorage.

--- www/js/config.js
+++ www/js/config.js
@@ -1,12 +1,17 @@
 define(['zepto'], function($) {
-  var speed = 5;
   var minspeed = 1;
   var maxspeed = 10;
+  var speed = parseInt(localStorage['speed']);
+
+  if (!(speed >= minspeed && speed <= maxspeed))
+    speed = 5;
 
   function changeSpeed(delta) {
     speed = Math.max(minspeed, Math.min(maxspeed, speed + delta));
+    localStorage['speed'] = speed;
     $('#speed').text(speed);
   }
+  changeSpeed(0); // initialize
 
   $('.changespeed').click(function(e) {
     e.preventDefault();
